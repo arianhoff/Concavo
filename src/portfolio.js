5359447bbf88ce11b7e486d32830edcb55a68f52
ArianHoffmann <arianhoffmann16@gmail.com>
2022-07-31 08:56:07 -0300

    Se agrega el portfolio

--- src/portfolio.js
+++ src/portfolio.js
@@ -1,6 +1,11 @@
 import React from 'react'
 import './portfolio.css'
 import Imagen from './assets/portfolio.jpg'
+import Sediferente from "./assets/sediferente.jpg"
+import Telohacemos from "./assets/telohacemos.jpg"
+import Sjdesarrollos from "./assets/sjdesarrollos.jpg"
+import Mesaune from "./assets/mesaune.jpg"
+import Sopladora from "./assets/sopladora.jpg"
 
 const portfolio = () => {
   return (
@@ -9,9 +14,9 @@ const portfolio = () => {
       <div className="fila">
         <div className="proyecto">
           <div className="overlay">
-            <img src={Imagen} alt="Imagen del Proyecto"/>
+            <img src={Sediferente} alt="Imagen del Proyecto"/>
             <div className="info">
-              <a href='https://www.behance.net/' target="blank">
+              <a href='https://www.behance.net/gallery/149126825/Social-Media-Se-diferente-academia' target="blank">
               <h4>Descripción Del Proyecto</h4>
               <p>Diseño Web</p>
               </a>
@@ -20,9 +25,44 @@ const portfolio = () => {
         </div>
         <div className="proyecto">
           <div className="overlay">
-            <img src={Imagen} alt="Imagen del Proyecto" />
+            <img src={Telohacemos} alt="Imagen del Proyecto" />
             <div className="info">
-            <a href='https://www.behance.net/' target="blank">
+            <a href='https://www.behance.net/gallery/149180367/Social-Media-Te-lo-hacemos-Facil' target="blank">
+              <h4>Descripción Del Proyecto</h4>
+              <p>Diseño Web</p>
+              </a>
+            </div>
+          </div>
+        </div>
+        <div className="proyecto">
+          <div className="overlay">
+            <img src={Sjdesarrollos} alt="Imagen del Proyecto" />
+            <div className="info">
+            <a href='https://www.behance.net/gallery/149185433/Social-Media-Sj-Desarrollos' target="blank">
+              <h4>Descripción Del Proyecto</h4>
+              <p>Diseño Web</p>
+              </a>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="fila">
+        <div className="proyecto">
+          <div className="overlay">
+            <img src={Mesaune} alt="Imagen del Proyecto" />
+            <div className="info">
+            <a href='https://www.behance.net/gallery/149128453/Mesa-Une-Industrial-Design' target="blank">
+              <h4>Descripción Del Proyecto</h4>
+              <p>Diseño Web</p>
+              </a>
+            </div>
+          </div>
+        </div>
+        <div className="proyecto">
+          <div className="overlay">
+            <img src={Sopladora} alt="Imagen del Proyecto" />
+            <div className="info">
+            <a href='https://www.behance.net/gallery/149363805/Diseno-Insdustrial-Sopladora-Greenworks' target="blank">
               <h4>Descripción Del Proyecto</h4>
               <p>Diseño Web</p>
               </a>
@@ -42,7 +82,7 @@ const portfolio = () => {
         </div>
       </div>
       <div className="fila">
-        <div className="proyecto">
+        {/* <div className="proyecto">
           <div className="overlay">
             <img src={Imagen} alt="Imagen del Proyecto" />
             <div className="info">
@@ -52,8 +92,8 @@ const portfolio = () => {
               </a>
             </div>
           </div>
-        </div>
-        <div className="proyecto">
+        </div> */}
+        {/* <div className="proyecto">
           <div className="overlay">
             <img src={Imagen} alt="Imagen del Proyecto" />
             <div className="info">
@@ -63,8 +103,8 @@ const portfolio = () => {
               </a>
             </div>
           </div>
-        </div>
-        <div className="proyecto">
+        </div> */}
+        {/* <div className="proyecto">
           <div className="overlay">
             <img src={Imagen} alt="Imagen del Proyecto" />
             <div className="info">
@@ -74,42 +114,7 @@ const portfolio = () => {
               </a>
             </div>
           </div>
-        </div>
-      </div>
-      <div className="fila">
-        <div className="proyecto">
-          <div className="overlay">
-            <img src={Imagen} alt="Imagen del Proyecto" />
-            <div className="info">
-            <a href='https://www.behance.net/' target="blank">
-              <h4>Descripción Del Proyecto</h4>
-              <p>Diseño Web</p>
-              </a>
-            </div>
-          </div>
-        </div>
-        <div className="proyecto">
-          <div className="overlay">
-            <img src={Imagen} alt="Imagen del Proyecto" />
-            <div className="info">
-            <a href='https://www.behance.net/' target="blank">
-              <h4>Descripción Del Proyecto</h4>
-              <p>Diseño Web</p>
-              </a>
-            </div>
-          </div>
-        </div>
-        <div className="proyecto">
-          <div className="overlay">
-            <img src={Imagen} alt="Imagen del Proyecto" />
-            <div className="info">
-            <a href='https://www.behance.net/' target="blank">
-              <h4>Descripción Del Proyecto</h4>
-              <p>Diseño Web</p>
-              </a>
-            </div>
-          </div>
-        </div>
+        </div> */}
       </div>
     </div>
   )
